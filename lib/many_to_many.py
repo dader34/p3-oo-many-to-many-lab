a142ff1d749390a25ae9ff8df033244c29ea2efe
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-27 10:23:10 -0700

    Passed all tests

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,90 +1,103 @@
 class Author:
-
-    all = []
-
-    def __init__(self, name):
-        self.name = name
-        Author.all.append(self)
+    def __init__(self,name):
+        self._name = name
 
     def contracts(self):
         return [contract for contract in Contract.all if contract.author == self]
 
     def books(self):
-        return [contract.book for contract in self.contracts()]
-
-    def sign_contract(self, book, date, royalties):
-        return Contract(self, book, date, royalties)
+        return [contract.book for contract in Contract.all if contract.author == self]
+    
+    def sign_contract(self,book,date,royalties):
+        return Contract(self,book,date,royalties)
     
     def total_royalties(self):
-        return sum([contract.royalties for contract in self.contracts()])
+        return sum([contract.royalties for contract in Contract.all if contract.author == self])
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,name):
+        if(type(name) == str):
+            self._name = name
 
 class Book:
+    def __init__(self,title):
+        self._title = title
 
-    all = []
-
-    def __init__(self, title):
-        self.title = title
-        Book.all.append(self)
-    
     def contracts(self):
         return [contract for contract in Contract.all if contract.book == self]
-
+    
     def authors(self):
-        return [contract.author for contract in self.contracts()]
+        return [contract.author for contract in Contract.all if contract.book == self]
+    
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self,title):
+        if(type(title) == str):
+            self._title = title
 
 class Contract:
 
     all = []
 
-    def __init__(self, author, book, date, royalties):
-        self.author = author
-        self.book = book
-        self.date = date
-        self.royalties = royalties
-        Contract.all.append(self)
+    def __init__(self,author,book,date,royalties):
+        self.author,self.book,self.date,self.royalties = author,book,date,royalties
+
+        self.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls,date):
+        return [contract for contract in cls.all if contract.date == date]
 
     @property
     def author(self):
         return self._author
     
     @author.setter
-    def author(self, value):
-        if not isinstance(value, Author):
-            raise Exception
-        self._author = value
+    def author(self,author):
+        if(isinstance(author,Author)):
+            self._author = author
+        else:
+            raise Exception("You cant do that")
 
     @property
     def book(self):
         return self._book
     
     @book.setter
-    def book(self, value):
-        if not isinstance(value, Book):
+    def book(self,book):
+        if(isinstance(book,Book)):
+            self._book = book
+        else:
             raise Exception
-        self._book = value
 
     @property
     def date(self):
         return self._date
     
     @date.setter
-    def date(self, value):
-        if not isinstance(value, str):
+    def date(self,date):
+        if(type(date) == str):
+            self._date = date
+        else:
             raise Exception
-        self._date = value
 
     @property
     def royalties(self):
         return self._royalties
-    
-    @royalties.setter
-    def royalties(self, value):
-        if not isinstance(value, int):
-            raise Exception
-        self._royalties = value
-
-    @classmethod
-    def contracts_by_date(cls, date):
-        return [contract for contract in cls.all if contract.date == date]
         
+    @royalties.setter
+    def royalties(self,royalties):
+        if(type(royalties) == int):
+            self._royalties = royalties
+        else:
+            raise Exception
+
+
